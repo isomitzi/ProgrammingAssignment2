2f3aaa181ede4b4459bde3ccd8ce3e5633d63d03
Israel <israel.touitou@gmail.com>
2014-11-19 22:57:09 +0200

    first changes
    added the function description

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## what i did was to mimic the thought pattern that was shown in the example
+## the first function will hold a place for the inverse matrix as well as nested function
+## the nested function will be able to get the original matrix, set it, get the inverse matrix
+##and set the inverse matrix.
+## the second function will check for a chached value. if it's there- the funtion will return it
+## else, the function will calculate the inverse matrix, store it in first function and will print it.
 
 ## Write a short comment describing this function
 
